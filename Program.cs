6d9acacde6ab746f6a6b29877c4d520c41e83cbc
Andres Sainz <andresmsainz@gmail.com>
2021-05-09 04:23:46 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InheritanceExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance Exercise!");
+
+            FourWheeledVehicles car = new FourWheeledVehicles();
+
+            car.ChangeColor();
+            car.StartVehicle();
+            car.RunAtFullSpeed();
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceExercise
+{
+    class Vehicle
+    {
+    }
+
+    class WheeledVehicles : Vehicle
+    {
+        private string color;
+        private decimal price;
+
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+
+        public void ChangeColor()
+        {
+            Console.WriteLine("Color has changed");
+        }
+    }
+
+    class FourWheeledVehicles : WheeledVehicles
+    {
+        private string brandName;
+        private int capacity;
+
+        public string BrandName { get; set; }
+        public int Capacity { get; set; }
+
+        public void StartVehicle()
+        {
+            Console.WriteLine("Four Wheel Vehicle has started");
+        }
+
+        public void RunAtFullSpeed()
+        {
+            Console.WriteLine("Four Wheel Vehicle is running at full speed");
+        }
+
+        public FourWheeledVehicles()
+        {
+
+        }
+    }
+}
